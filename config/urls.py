a14a22f322505925b75742f913fd3fa6372432fe
Almaz Yessenyazov <mr.esenyazov@gmail.com>
2021-05-21 22:21:40 +0600

    Created urls architecture

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include([
+        # Version 1 of API
+        path('v1/', include([
+            # Authorization, User related apis
+            path('auth/', include('src.accounts.urls')),
+
+            # Another apis
+            # ...
+        ]))
+    ])),
 ]
