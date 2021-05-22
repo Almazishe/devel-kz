a8bc398e3965a12ca7527376e41bd911ff88514d
Dice <dicenameismy@gmail.com>
2021-05-22 14:10:04 +0600

    create and migrate new user model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import CustomUser
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'phone_number',)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """ Rewrote admin to show all fields
+    we are interested in.
+    
+    """
+    
+    list_display = ('username', 'email', 'phone', 'first_name', 'last_name', 'is_staff')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    search_fields = ('username', 'email', 'phone')
+    ordering = ('username', 'email', 'phone')
