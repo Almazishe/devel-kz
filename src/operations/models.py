6eb0c3f2ced3b1bca83e0ed85f326b7dddcad301
Lakhaibekov <lnurbol.007@gmail.com>
2021-05-22 14:50:46 +0600

    Модель Покупатель и Проект

--- src/operations/models.py
+++ src/operations/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from utils.models import BaseModel, DateModel, Deletable
-
+from django.contrib.auth.models import User
 
 class IncomeCategory(models.Model):
     name = models.CharField("Название", max_length=255)
@@ -105,6 +105,57 @@ class Outcome(BaseModel, DateModel, Deletable):
     class Meta:
         verbose_name = 'Расход'
         verbose_name_plural = 'Расходы'
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Customer(models.Model):
+    name = models.CharField("Название", max_length=255)
+    description = models.TextField("Описание", null=True, blank=True)
+    phone = models.TextField("Описание", null=True, blank=True)
+    email = models.TextField("Описание", null=True, blank=True)
+    website = models.TextField("Описание", null=True, blank=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        verbose_name = "Покупатель проекта"
+        verbose_name_plural = "Покупатели проекта"
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Projects(BaseModel, DateModel, Deletable):
+    """
+        Проект
+    """
+
+    customer = models.ForeignKey(
+        Customer,
+        verbose_name="Покупатель",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    developers = models.ManyToManyField(User)
+    payments = models.ManyToManyField(Income)
+    expences = models.ManyToManyField(Outcome)
+
+    title = models.CharField("Заголовок", max_length=255)
+    description = models.TextField("Описание", null=True, blank=True)
+    technical_task = models.FileField(upload_to='files/tasks', blank=True)
+    price = models.FloatField("Цена")
+    status = models.CharField("Статус", max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    is_delated = models.BooleanField("Оплачено", default=False)
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
 
     def __str__(self):
         return f'{self.title}'
