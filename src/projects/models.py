0d46df47a43f4f9aa7d97475f2eb93fcc90d3f58
Dice <dicenameismy@gmail.com>
2021-05-25 13:00:44 +0600

    FIX and UPGRADE models i did not create

--- src/projects/models.py
+++ src/projects/models.py
@@ -5,47 +5,57 @@ from operations.models import Income, Outcome
 
 
 # Create your models here.
-class Customer(models.Model):
+class Customer(DateModel, Deletable):
+    """
+        Модель заказщика со всеми имеющимися контактыми данными
+    """
+
     name = models.CharField("Название", max_length=255)
-    description = models.TextField("Описание", null=True, blank=True)
-    phone = models.CharField("Телефон", max_length=12)
-    email = models.EmailField("Почта", null=True, blank=True)
-    website = models.CharField("Веб-сайт", max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField("Описание", blank=True, default='')
+    phone = models.CharField("Телефон", max_length=12, blank=True)
+    email = models.EmailField("Почта", blank=True)
+    website = models.CharField("Веб-сайт", max_length=255, blank=True)
 
     class Meta:
-        verbose_name = "Покупатель проекта"
-        verbose_name_plural = "Покупатели проекта"
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
 
     def __str__(self):
         return f'{self.name}'
 
 
-class Projects(models.Model):
+class Project(BaseModel, DateModel, Deletable):
     """
         Проект
     """
+    PROCESSING = 'PR'
+    UNDER_DEV = 'UD'
+    FINISHED = 'FN'
+
+    PROJECT_CTATUS_CHOICES = [
+        (PROCESSING, 'Processing'),
+        (UNDER_DEV, 'Under development'),
+        (FINISHED, 'Finished'),
+    ]
 
     customer = models.ForeignKey(
         Customer,
         verbose_name="Покупатель",
         on_delete=models.SET_NULL,
-        null=True,
-        blank=True
+        null=True
     )
-    developers = models.ManyToManyField(get_user_model())
-    payments = models.ManyToManyField(Income)
-    expences = models.ManyToManyField(Outcome)
+    developers = models.ManyToManyField(get_user_model(), blank=True, related_name='project')
+    payments = models.ManyToManyField(Income, blank=True, related_name='project')
+    expences = models.ManyToManyField(Outcome, blank=True, related_name='project')
 
     title = models.CharField("Заголовок", max_length=255)
-    description = models.TextField("Описание", null=True, blank=True)
+    description = models.TextField("Описание", blank=True, default='')
     technical_task = models.FileField(upload_to='files/project_tasks', blank=True)
+    status = models.CharField("Статус", choices=PROJECT_CTATUS_CHOICES, max_length=2)
     price = models.FloatField("Цена")
-    status = models.CharField("Статус", max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    is_delated = models.BooleanField("Оплачено", default=False)
+
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
 
     class Meta:
         verbose_name = 'Проект'
