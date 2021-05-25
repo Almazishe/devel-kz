8ac31303595eb3e554d6de129f96ef95a2082d69
Dice <dicenameismy@gmail.com>
2021-05-25 09:03:45 +0600

    delete moved models

--- src/operations/models.py
+++ src/operations/models.py
@@ -111,56 +111,6 @@ class Outcome(BaseModel, DateModel, Deletable):
         return f'{self.title}'
 
 
-class Customer(models.Model):
-    name = models.CharField("Название", max_length=255)
-    description = models.TextField("Описание", null=True, blank=True)
-    phone = models.TextField("Описание", null=True, blank=True)
-    email = models.TextField("Описание", null=True, blank=True)
-    website = models.TextField("Описание", null=True, blank=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-
-    class Meta:
-        verbose_name = "Покупатель проекта"
-        verbose_name_plural = "Покупатели проекта"
-
-    def __str__(self):
-        return f'{self.name}'
-
-
-class Projects(BaseModel, DateModel, Deletable):
-    """
-        Проект
-    """
-
-    customer = models.ForeignKey(
-        Customer,
-        verbose_name="Покупатель",
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True
-    )
-    developers = models.ManyToManyField(get_user_model())
-    payments = models.ManyToManyField(Income)
-    expences = models.ManyToManyField(Outcome)
-
-    title = models.CharField("Заголовок", max_length=255)
-    description = models.TextField("Описание", null=True, blank=True)
-    technical_task = models.FileField(upload_to='files/tasks', blank=True)
-    price = models.FloatField("Цена")
-    status = models.CharField("Статус", max_length=255)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    is_delated = models.BooleanField("Оплачено", default=False)
-
-    class Meta:
-        verbose_name = 'Проект'
-        verbose_name_plural = 'Проекты'
-
-    def __str__(self):
-        return f'{self.title}'
-
-
 class Resume(models.Model):
     """ 
     Модель резюме отправленных через главную страницу сайта
