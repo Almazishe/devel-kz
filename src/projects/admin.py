e81cccf1fc00fa337680afac6d70962549301ccc
Dice <dicenameismy@gmail.com>
2021-05-25 13:01:05 +0600

    add project and customer models to admin

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Project
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
